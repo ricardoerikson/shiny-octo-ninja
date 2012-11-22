a287f9a9bda69b9e6588b7c26e40e8c997759eda
Ricardo Erikson <ricardoerikson@gmail.com>
2012-11-22 14:27:07 -0400

    ggregation_parameters.m bug fix

--- aggregation_parameters.m
+++ aggregation_parameters.m
@@ -4,20 +4,20 @@
 %   neighbors (vector)
 %   sim_uv (vector)
 
-function [ r_ubar,r_vbar, r_vi, sim_uv] = aggregation_parameters( user,neighbors,ratings_matrix,similarity_matrix )
+function [ r_ubar,r_vbar, r_vi, sim_uv] = aggregation_parameters( user,item,neighbors,ratings_matrix,similarity_matrix )
 
 r_ubar = user_mean(ratings_matrix(user,:));
 n=max(size(neighbors));
 
 r_vbar = zeros(size(neighbors));
 sim_uv = zeros(size(neighbors));
+r_vi   = zeros(size(neighbors));
 
 for i=1:n
     r_vbar(i) = user_mean(ratings_matrix(neighbors(i),:));
     sim_uv(i) = similarity(user,neighbors(i),similarity_matrix);
+    r_vi(i)   = ratings_matrix(neighbors(i),item);
 end
-
-r_vi = neighbors;
 
 end
 
