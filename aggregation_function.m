318de4123b1c56c04a05ccb5f913dbcf51646c3e
Ricardo Erikson <ricardoerikson@gmail.com>
2012-11-22 11:36:41 -0400

    Refactoring in aggregation function

--- aggregation_function.m
+++ aggregation_function.m
@@ -1,7 +1,12 @@
+% r_ubar (value): mean for user u
+% r_vi (vector): rating from user v for item i
+% r_vbar (vector): mean for user v
+% sim_uv (vector): similarity between user u and v
+
 function [prediction] = aggregation_function(r_ubar,r_vi,r_vbar,sim_uv)
 
-diff = r_vi - ones(1,max(size(r_vi)))*r_vbar;
-sum_part = sim*diff';
-k = 1/sum(sim_uv);
+diff = r_vi - r_vbar;
+sum_part = dot(sim_uv,diff);
+k = 1/sum(abs(sim_uv));
 
 prediction = r_ubar + k*sum_part;
