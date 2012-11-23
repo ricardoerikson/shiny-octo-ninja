deb6bad6bd14b40fffa50048fc8ce9b7412a12f0
Ricardo Erikson <ricardoerikson@gmail.com>
2012-11-23 15:47:30 -0400

    Rename internal and return variables of function expand_neighborhood.m

--- expand_neighborhood.m
+++ expand_neighborhood.m
@@ -1,4 +1,4 @@
-function [knn,nn_knn,nn_weight] = expand_neighborhood(k,nn_size,user,item,similarity_matrix,ratings_matrix)
+function [knn,ex_knn,ex_weight] = expand_neighborhood(k,ex_size,user,item,similarity_matrix,ratings_matrix)
 
 weight_similarity = zeros(1,size(ratings_matrix,1));
 
@@ -12,19 +12,19 @@ for i=1:max(size(knn))
     nn_neighborhood_vector = ratings_matrix(:,item) > 0;
     nn_similarity_vector = similarity_matrix(knn(i),:);
     
-    [nn_knn] = nearest_neighbors(k,nn_similarity_vector,nn_neighborhood_vector);
+    [ex_knn] = nearest_neighbors(k,nn_similarity_vector,nn_neighborhood_vector);
     
-    weight_similarity(nn_knn) = weight_similarity(nn_knn) + 1;
+    weight_similarity(ex_knn) = weight_similarity(ex_knn) + 1;
     
 end;
 
 [sortedValues,sortIndex] = sort(weight_similarity(:),'descend');
-nn_weight = sortedValues(sortedValues > 0);
-nn_knn = sortIndex(sortedValues > 0);
+ex_weight = sortedValues(sortedValues > 0);
+ex_knn = sortIndex(sortedValues > 0);
 
-if (max(size(nn_knn))>nn_size)
-    nn_knn = nn_knn(1:nn_size);
-    nn_weight = nn_weight(1:nn_size);
+if (max(size(ex_knn))>ex_size)
+    ex_knn = ex_knn(1:ex_size);
+    ex_weight = ex_weight(1:ex_size);
 end;
 
 end
