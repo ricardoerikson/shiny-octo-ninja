fcf84ff1dc56fd407818b3784de22c96176cc152
Ricardo Erikson <ricardoerikson@gmail.com>
2012-11-25 00:44:59 -0400

    1. Bug fix in predict_rating_weighted.m function 2. Fix for when knn is equals to 0 in expand_neighborhood.m and predict_ratings_weighted.m functions 3. Main program to predict ratings in 3 ways:    - Standard aggregation function using kNN (highest pearson correlation)    - Aggregation function using expanded neighborhood (most repeated neighbors of the neighbors)    - Aggregation function using expanded neighborhood + weight (#(repeated neighbors)))

--- expand_neighborhood.m
+++ expand_neighborhood.m
@@ -4,21 +4,27 @@ weight_similarity = zeros(1,size(ratings_matrix,1));
 
 [knn] = nearest_neighbors(k,user,item,similarity_matrix,ratings_matrix);
 
-for i=1:max(size(knn))
-    
-    [ex_knn] = nearest_neighbors(k,knn(i),item,similarity_matrix,ratings_matrix);
-    
-    weight_similarity(ex_knn) = weight_similarity(ex_knn) + 1;
-    
+if(min(size(knn))>0)
+	for i=1:max(size(knn))
+	    
+	    [ex_knn] = nearest_neighbors(k,knn(i),item,similarity_matrix,ratings_matrix);
+	    
+	    weight_similarity(ex_knn) = weight_similarity(ex_knn) + 1;
+	    
+	end;
+
+	[sortedValues,sortIndex] = sort(weight_similarity(:),'descend');
+	ex_weight = sortedValues(sortedValues > 0);
+	ex_knn = sortIndex(sortedValues > 0);
+
+	if (max(size(ex_knn))>ex_size)
+	    ex_knn = ex_knn(1:ex_size);
+	    ex_weight = ex_weight(1:ex_size);
+	end;
+else
+	ex_knn = knn;
+	ex_weight = knn;
 end;
 
-[sortedValues,sortIndex] = sort(weight_similarity(:),'descend');
-ex_weight = sortedValues(sortedValues > 0);
-ex_knn = sortIndex(sortedValues > 0);
-
-if (max(size(ex_knn))>ex_size)
-    ex_knn = ex_knn(1:ex_size);
-    ex_weight = ex_weight(1:ex_size);
-end;
 
 end
--- main_recommendation.m
+++ main_recommendation.m
@@ -1,1 +1,28 @@
- 
+clear;
+clc;
+
+data = load('dataset/ua_base_data.mat');
+raw_data = load('dataset/ua.test');
+
+test_data = raw_data(:,1:3);
+clear raw_data;
+
+real_value = test_data(:,3);
+prediction.std = zeros(1,max(size(test_data)));
+prediction.ext = zeros(1,max(size(test_data)));
+prediction.ext_wtd = zeros(1,max(size(test_data)));
+
+
+for i=1:max(size(test_data))
+	[knn,ex_knn,ex_weight]=expand_neighborhood(10,10,test_data(i,1),test_data(i,2),data.correlation_matrix,data.ratings_matrix);
+
+	if(min(size(knn))>0)
+		[st.r_ubar,st.r_vbar,st.r_vi,st.sim_uv] = get_aggregation_par(test_data(i,1),test_data(i,2),knn,data.ratings_matrix,data.correlation_matrix);
+		[ext.r_ubar,ext.r_vbar,ext.r_vi,ext.sim_uv] = get_aggregation_par(test_data(i,1),test_data(i,2),ex_knn,data.ratings_matrix,data.correlation_matrix);
+
+		prediction.std(i) = predict_rating(st.r_ubar,st.r_vi,st.r_vbar,st.sim_uv);
+		prediction.ext(i) = predict_rating(ext.r_ubar,ext.r_vi,ext.r_vbar,ext.sim_uv);
+		prediction.ext_wtd(i) = predict_rating_weighted(ext.r_ubar,ext.r_vi,ext.r_vbar,ext.sim_uv,ex_weight);
+	end;
+
+end;
