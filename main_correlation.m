f2805f473663f8e45ab8eecb406844fbfaa8a64d
Ricardo Erikson <ricardoerikson@gmail.com>
2012-11-26 15:14:50 -0400

    Change in main_correlation.m function

--- main_correlation.m
+++ main_correlation.m
@@ -5,17 +5,17 @@ clc;
 n_users = 943;
 n_movies = 1682;
 
-data = load_data('dataset/ua.base',n_users,n_movies);
+ratings_matrix = load_data('dataset/ua.base',n_users,n_movies);
 
 correlation_matrix = zeros(n_users,n_users);
 
 parfor i=1:n_users
-    vector_i = data(i,:);
+    vector_i = ratings_matrix(i,:);
     for j=1:n_users
         if(i~=j)
-            correlation_matrix(i,j) = pearson_correlation(vector_i,data(j,:));
+            correlation_matrix(i,j) = pearson_correlation(vector_i,ratings_matrix(j,:));
         end
     end;
 end;
 
-save('dataset/pearson_correlation_base.mat','correlation_matrix','-mat');
+save('dataset/ua_base_data.mat','correlation_matrix','ratings_matrix','-mat');
