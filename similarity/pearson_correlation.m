cad5dbbbb845ca745bfc0097dcdb145d7269e6a1
Ricardo Erikson <ricardoerikson@gmail.com>
2012-11-26 15:10:55 -0400

    Bug fix in pearson_correlation.m function when denominator is equals 0.

--- similarity/pearson_correlation.m
+++ similarity/pearson_correlation.m
@@ -7,10 +7,17 @@ if(min(size(common))>0)
     mean_a = user_mean(vector_a);
     mean_b = user_mean(vector_b);
 
-    diff_a = vector_a(common) - ones(min(size(common)),max(size(common)))*mean_a;
-    diff_b = vector_b(common) - ones(min(size(common)),max(size(common)))*mean_b;
+    diff_a = vector_a(row(common)) - ones(size(row(common)))*mean_a;
+    diff_b = vector_b(row(common)) - ones(size(row(common)))*mean_b;
 
-    value = (diff_a*diff_b')/(sqrt(sum(diff_a.^2))*sqrt(sum(diff_b.^2)));
+    norm_a = norm(diff_a);
+    norm_b = norm(diff_b);
+
+    if(norm_a ~= 0 && norm_b ~= 0)
+	    value = dot(diff_a,diff_b)/(norm_a*norm_b);
+	else
+		value = 0;
+    end
 else
     value = 0;
 end;
