0580c4f79c571385a1b95edcc23f8be50add414b
Ricardo Erikson <ricardoerikson@gmail.com>
2012-11-28 00:07:18 -0400

    Redefinition of the vector that stores the prediction values.

--- main_experiment_std_exp_wgt.m
+++ main_experiment_std_exp_wgt.m
@@ -10,40 +10,36 @@ raw_data = load('dataset/ua.test');
 test_data = raw_data(:,1:3);
 clear raw_data;
 
-nn_size = [5 10 15 20 25];
+nn_values = [5 10 15 20 25];
+exp_values = 4:2:30;
 
-prediction_std = zeros(max(size(test_data)),max(size(nn_size)),max(size(nn_size)));
-prediction_exp = zeros(max(size(test_data)),max(size(nn_size)),max(size(nn_size)));
-prediction_wgt = zeros(max(size(test_data)),max(size(nn_size)),max(size(nn_size)));
+s_nn = max(size(nn_values));
+s_ex = max(size(exp_values));
+s_ts = max(size(test_data));
+
+real_values = test_data(:,3);
+
+predict_std = zeros(s_ts,s_ex,s_nn);
+predict_exp = zeros(s_ts,s_ex,s_nn);
+predict_wgt = zeros(s_ts,s_ex,s_nn);
 
 % i = size(knn): k neares neighbors size;
 % j = size(exp_knn): expanded neighborhood size;
-for i=1:max(size(nn_size))
-	for j=1:max(size(nn_size))
+for i=1:max(size(nn_values))
+	for j=1:max(size(exp_values))
 		parfor k=1:max(size(test_data))
-			[knn,ex_knn,ex_weight]=expand_neighborhood(nn_size(i),nn_size(j),test_data(k,1),test_data(k,2),data.correlation_matrix,data.ratings_matrix);
+			[knn,ex_knn,ex_weight]=expand_neighborhood(nn_values(i),exp_values(j),test_data(k,1),test_data(k,2),data.correlation_matrix,data.ratings_matrix);
 
 			if(min(size(knn))>0)
 				[st_r_ubar,st_r_vbar,st_r_vi,st_sim_uv] = get_aggregation_par(test_data(k,1),test_data(k,2),knn,data.ratings_matrix,data.correlation_matrix);
 				[ext_r_ubar,ext_r_vbar,ext_r_vi,ext_sim_uv] = get_aggregation_par(test_data(k,1),test_data(k,2),ex_knn,data.ratings_matrix,data.correlation_matrix);
 
-				prediction_std(k,i,j) = predict_rating(st_r_ubar,st_r_vi,st_r_vbar,st_sim_uv);
-				prediction_exp(k,i,j) = predict_rating(ext_r_ubar,ext_r_vi,ext_r_vbar,ext_sim_uv);
-				prediction_wgt(k,i,j) = predict_rating_weighted(ext_r_ubar,ext_r_vi,ext_r_vbar,ext_sim_uv,ex_weight);
+				predict_std(k,j,i) = predict_rating(st_r_ubar,st_r_vi,st_r_vbar,st_sim_uv);
+				predict_exp(k,j,i) = predict_rating(ext_r_ubar,ext_r_vi,ext_r_vbar,ext_sim_uv);
+				predict_wgt(k,j,i) = predict_rating_weighted(ext_r_ubar,ext_r_vi,ext_r_vbar,ext_sim_uv,ex_weight);
 			end;
 		end;
 	end;
 end;
 
-% pos_ext = prediction_exp > 0;
-% pos_wgt = prediction_wgt > 0;
-
-% rmse_std = rmse(real_value,prediction_std);
-% rmse_ext = rmse(real_value(pos_ext),prediction_exp(pos_ext));
-% rmse_wgt = rmse(real_value(pos_wgt),prediction_wgt(pos_wgt));
-
-% mae_std = mae(real_value,prediction_std);
-% mae_ext = mae(real_value(pos_ext),prediction_exp(pos_ext));
-% mae_wgt = mae(real_value(pos_wgt),prediction_wgt(pos_wgt));
-
-clear data pos_wgt pos_ext real_value test_data;
+save('dataset/ua_test_predictions_experiment_std_exp_wgt.mat','real_values','predict_std','predict_exp','predict_wgt','-mat');
