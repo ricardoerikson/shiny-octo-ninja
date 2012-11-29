8df7874bd917865827794ea8025f8b4a8a8e6e1a
Ricardo Erikson <ricardoerikson@gmail.com>
2012-11-29 11:11:23 -0400

    Correlation function added.

--- main_correlation.m
+++ main_correlation.m
@@ -5,17 +5,6 @@ clc;
 n_users = 943;
 n_movies = 1682;
 
-ratings_matrix = load_data('dataset/ua.base',n_users,n_movies);
-
-correlation_matrix = zeros(n_users,n_users);
-
-parfor i=1:n_users
-    vector_i = ratings_matrix(i,:);
-    for j=1:n_users
-        if(i~=j)
-            correlation_matrix(i,j) = pearson_correlation(vector_i,ratings_matrix(j,:));
-        end
-    end;
-end;
+[correlation_matrix,ratings_matrix]=correlation('dataset/ua.base',n_users,n_movies,0);
 
 save('dataset/ua_base_data_experiment.mat','correlation_matrix','ratings_matrix','-mat');
