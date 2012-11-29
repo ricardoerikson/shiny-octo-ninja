24721cc81e7d20b78c75af59423774b61f974f21
Ricardo Erikson <ricardoerikson@gmail.com>
2012-11-29 11:32:37 -0400

    Configuration script added to include paths (addpath).

---
+++ configure.m
@@ -0,0 +1,1 @@
+addpath('similarity','evaluation','utils');
--- main_correlation_u_data.m
+++ main_correlation_u_data.m
@@ -2,6 +2,8 @@ clear('all');
 close all;
 clc;
 
+configure
+
 [correlation_matrix,ratings_matrix]=correlation('dataset/u.data',943,1682,0);
 
 save('dataset/u_data_pearson_correlation.mat','correlation_matrix','ratings_matrix','-mat');
