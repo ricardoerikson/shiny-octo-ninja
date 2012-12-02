71c347d5a91b6c2539dcbaa98cec4254b2d57227
Ricardo Erikson <ricardoerikson@gmail.com>
2012-12-01 23:32:57 -0400

    Calculating correlation using the training set.

--- main_experiment_1_u_data_ratings.m
+++ main_experiment_1_u_data_ratings.m
@@ -4,29 +4,3 @@ clc;
 
 configure
 
-data = load('dataset/u_data_pearson_correlation.mat');
-
-n_users = 943;
-n_items = 1682;
-
-probe_set = cell(n_users,1);
-training_set = cell(n_users,1);
-
-parfor i=1:n_users
-
-	all_observed = find(data.ratings_matrix(i,:) > 0);
-	observed_positions = data.ratings_matrix(i,:) > 0;
-	all_values = data.ratings_matrix(i,observed_positions);
-
-	[sortedValues,sortedIndexes] = sort(all_values,'descend');
-	all_observed_sorted = all_observed(sortedIndexes);
-
-	n_observed = max(size(all_observed_sorted));
-	n_half = round(0.5*n_observed);
-	xseq = randperm(n_half);
-	n_probe = round(0.4*n_half);
-
-	probe_set{i} = all_observed_sorted(xseq(1:n_probe));
-	training_set{i} = cat(2,all_observed_sorted(xseq(n_probe+1:end)),all_observed_sorted(n_half+1:end));
-
-end;
