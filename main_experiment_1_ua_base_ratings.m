ed604809d25b7083943a11aebb851b498a2ada16
Ricardo Erikson <ricardoerikson@gmail.com>
2012-12-02 23:59:41 -0400

    predict_rating function reintegrated with get_aggregation_par function

--- main_experiment_1_ua_base_ratings.m
+++ main_experiment_1_ua_base_ratings.m
@@ -45,12 +45,13 @@ for i=1:max(size(nn_values))
 				sum_nn = sum_nn + mean_similarity(test_data(k,1),knn,data.correlation_matrix);
 				sum_ex = sum_ex + mean_similarity(test_data(k,1),ex_knn,data.correlation_matrix);
 
-				[st_r_ubar,st_r_vbar,st_r_vi,st_sim_uv] = get_aggregation_par(test_data(k,1),test_data(k,2),knn,data.ratings_matrix,data.correlation_matrix);
-				[ext_r_ubar,ext_r_vbar,ext_r_vi,ext_sim_uv] = get_aggregation_par(test_data(k,1),test_data(k,2),ex_knn,data.ratings_matrix,data.correlation_matrix);
+				std_rating = predict_rating(test_data(k,1),test_data(k,2),knn,data.ratings_matrix,data.correlation_matrix);
+				exp_rating = predict_rating(test_data(k,1),test_data(k,2),ex_knn,data.ratings_matrix,data.correlation_matrix);
+				wgt_rating = predict_rating_weighted(test_data(k,1),test_data(k,2),ex_knn,data.ratings_matrix,data.correlation_matrix,ex_weight);
 
-				predict_std(k,j,i) = predict_rating(st_r_ubar,st_r_vi,st_r_vbar,st_sim_uv);
-				predict_exp(k,j,i) = predict_rating(ext_r_ubar,ext_r_vi,ext_r_vbar,ext_sim_uv);
-				predict_wgt(k,j,i) = predict_rating_weighted(ext_r_ubar,ext_r_vi,ext_r_vbar,ext_sim_uv,ex_weight);
+				predict_std(k,j,i) = std_rating;
+				predict_exp(k,j,i) = exp_rating;
+				predict_wgt(k,j,i) = wgt_rating;
 			end;
 		end;
 		% 1: knn mean
