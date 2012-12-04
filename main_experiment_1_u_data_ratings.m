fa9f5594170a62e546e78dde15c3fb7f3712c145
Ricardo Erikson <ricardoerikson@gmail.com>
2012-12-04 18:17:16 -0400

    main_experiment_1_u_data_correlation.m to run standard prediction

--- main_experiment_1_u_data_ratings.m
+++ main_experiment_1_u_data_ratings.m
@@ -4,23 +4,25 @@ clc;
 
 configure
 
+start = clock;
+
 load('dataset/u_data_experiment_1.mat');
 
 n_users = 943;
 n_items = 1682;
 
-nn_values = [ 5 10 15 20 25 ];
-ex_values = 4:2:30;
+nn_values = [ 5 15 25 ];
+ex_values = 5:5:30;
 
 s_nn = max(size(nn_values));
 s_ex = max(size(ex_values));
 
 % STANDARD PREDICTION
 
-predictions_std = cell(s_ex,s_nn);
-
 for nn=1:s_nn
+	predictions_std = cell(s_ex,1);
 	for ex=1:s_ex
+		disp(['std -- ',num2str(nn),' x ',num2str(ex)]);
 		predictions = sparse(zeros(n_users,n_items));
 		parfor i=1:n_users
 			for j=1:n_items
@@ -30,62 +32,63 @@ for nn=1:s_nn
 					predictions(i,j) = predict_rating(i,j,knn,training_ratings,training_correlation);
 				end;
 			end;
-			disp(['std -- ',num2str(nn),' x ',num2str(ex),' user-',num2str(i)]);
+			
 		end;
-		predictions_std{ex,nn} = predictions;
+		predictions_std{ex} = predictions;
 		clear predictions;
 	end;
+	save(['dataset/experiment1/u_data_predictions_std',num2str(nn),'.mat'],'predictions_std','-mat');
+	clear predictions_std;
 end;
-
-save('dataset/experiment1/std/u_data_predictions_std.mat','predictions_std','-mat');
-clear predictions_std;
-
-% EXPANDED PREDICTION
-
-predictions_wgt = cell(s_ex,s_nn);
-
-for nn=1:s_nn
-	for ex=1:s_ex
-		predictions = sparse(zeros(n_users,n_items));
-		parfor i=1:n_users
-			for j=1:n_items
-
-				[knn,ex_knn,ex_weight]=expand_neighborhood(nn_values(nn),ex_values(ex),i,j,training_correlation,training_ratings);
-				if(max(size(knn))>0)
-					predictions(i,j) = predict_rating(i,j,ex_knn,training_ratings,training_correlation,ex_weight);
-				end;
-			end;
-			disp(['wgt -- ',num2str(nn),' x ',num2str(ex),' user-',num2str(i)]);
-		end;
-		predictions_wgt{ex,nn} = predictions;
-		clear predictions;
-	end;
-end;
-
-save('dataset/experiment1/wgt/u_data_predictions_wgt.mat','predictions_wgt','-mat');
-clear predictions_wgt;
 
 % WEIGHTED PREDICTION
 
-predictions_exp = cell(s_ex,s_nn);
+% predictions_exp = cell(s_ex,s_nn);
 
-for nn=1:s_nn
-	for ex=1:s_ex
-		predictions = sparse(zeros(n_users,n_items));
-		parfor i=1:n_users
-			for j=1:n_items
+% for nn=1:s_nn
+% 	for ex=1:s_ex
+% 		predictions = sparse(zeros(n_users,n_items));
+% 		parfor i=1:n_users
+% 			for j=1:n_items
 
-				[knn,ex_knn,ex_weight]=expand_neighborhood(nn_values(nn),ex_values(ex),i,j,training_correlation,training_ratings);
-				if(max(size(knn))>0)
-					predictions(i,j) = predict_rating(i,j,ex_knn,training_ratings,training_correlation);
-				end;
-			end;
-			disp(['exp -- ',num2str(nn),' x ',num2str(ex),' user-',num2str(i)]);
-		end;
-		predictions_exp{ex,nn} = predictions;
-		clear predictions;
-	end;
-end;
+% 				[knn,ex_knn,ex_weight]=expand_neighborhood(nn_values(nn),ex_values(ex),i,j,training_correlation,training_ratings);
+% 				if(max(size(knn))>0)
+% 					predictions(i,j) = predict_rating(i,j,ex_knn,training_ratings,training_correlation);
+% 				end;
+% 			end;
+% 			disp(['exp -- ',num2str(nn),' x ',num2str(ex),' user-',num2str(i)]);
+% 		end;
+% 		predictions_exp{ex,nn} = predictions;
+% 		clear predictions;
+% 	end;
+% end;
 
-save('dataset/experiment1/exp/u_data_predictions_exp.mat','predictions_exp','-mat');
-clear predictions_exp;
+% save('dataset/experiment1/u_data_predictions_exp.mat','predictions_exp','-mat');
+% clear predictions_exp;
+
+% % EXPANDED PREDICTION
+
+% predictions_wgt = cell(s_ex,s_nn);
+
+% for nn=1:s_nn
+% 	for ex=1:s_ex
+% 		predictions = sparse(zeros(n_users,n_items));
+% 		parfor i=1:n_users
+% 			for j=1:n_items
+
+% 				[knn,ex_knn,ex_weight]=expand_neighborhood(nn_values(nn),ex_values(ex),i,j,training_correlation,training_ratings);
+% 				if(max(size(knn))>0)
+% 					predictions(i,j) = predict_rating(i,j,ex_knn,training_ratings,training_correlation,ex_weight);
+% 				end;
+% 			end;
+% 			disp(['wgt -- ',num2str(nn),' x ',num2str(ex),' user-',num2str(i)]);
+% 		end;
+% 		predictions_wgt{ex,nn} = predictions;
+% 		clear predictions;
+% 	end;
+% end;
+
+% save('dataset/experiment1/u_data_predictions_wgt.mat','predictions_wgt','-mat');
+% clear predictions_wgt;
+
+finish = clock;
