2003bd3906c98e5dcb77ec682e5103af4f7cd01b
Ricardo Erikson <ricardoerikson@gmail.com>
2012-12-04 18:36:29 -0400

    main_experiment_1_u_data_ratings_wgt.m to run weighted prediction

--- main_experiment_1_u_data_ratings_wgt.m
+++ main_experiment_1_u_data_ratings_wgt.m
@@ -17,28 +17,28 @@ ex_values = 5:5:30;
 s_nn = max(size(nn_values));
 s_ex = max(size(ex_values));
 
-% EXPANDED PREDICTION
+% WEIGHTED PREDICTION
 
 for nn=1:s_nn
-	predictions_exp = cell(s_ex,1);
+	predictions_wgt = cell(s_ex,1);
 	for ex=1:s_ex
-		disp(['exp -- ',num2str(nn),' x ',num2str(ex)]);
+		disp(['wgt -- ',num2str(nn),' x ',num2str(ex)]);
 		predictions = sparse(zeros(n_users,n_items));
 		parfor i=1:n_users
 			for j=1:n_items
 
 				[knn,ex_knn,ex_weight]=expand_neighborhood(nn_values(nn),ex_values(ex),i,j,training_correlation,training_ratings);
 				if(max(size(knn))>0)
-					predictions(i,j) = predict_rating(i,j,ex_knn,training_ratings,training_correlation);
+					predictions(i,j) = predict_rating(i,j,ex_knn,training_ratings,training_correlation,ex_weight);
 				end;
 			end;
 			
 		end;
-		predictions_exp{ex} = predictions;
+		predictions_wgt{ex} = predictions;
 		clear predictions;
 	end;
-	save(['dataset/experiment1/u_data_predictions_exp',num2str(nn),'.mat'],'predictions_exp','-mat');
-	clear predictions_exp;
+	save(['dataset/experiment1/u_data_predictions_wgt',num2str(nn),'.mat'],'predictions_wgt','-mat');
+	clear predictions_wgt;
 end;
 
 finish = clock;
