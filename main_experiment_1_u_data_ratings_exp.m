91d984235075b611c06d255a5a3999822c9a4aa5
Ricardo Erikson <ricardoerikson@gmail.com>
2012-12-04 18:29:58 -0400

    main_experiment_1_u_data_ratings_exp.m to run expanded prediction

---
+++ main_experiment_1_u_data_ratings_exp.m
@@ -0,0 +1,44 @@
+close all;
+clear('all');
+clc;
+
+configure
+
+start = clock;
+
+load('dataset/u_data_experiment_1.mat');
+
+n_users = 943;
+n_items = 1682;
+
+nn_values = [ 5 15 25 ];
+ex_values = 5:5:30;
+
+s_nn = max(size(nn_values));
+s_ex = max(size(ex_values));
+
+% WEIGHTED PREDICTION
+
+for nn=1:s_nn
+	predictions_exp = cell(s_ex,1);
+	for ex=1:s_ex
+		disp(['exp -- ',num2str(nn),' x ',num2str(ex)]);
+		predictions = sparse(zeros(n_users,n_items));
+		parfor i=1:n_users
+			for j=1:n_items
+
+				[knn,ex_knn,ex_weight]=expand_neighborhood(nn_values(nn),ex_values(ex),i,j,training_correlation,training_ratings);
+				if(max(size(knn))>0)
+					predictions(i,j) = predict_rating(i,j,ex_knn,training_ratings,training_correlation);
+				end;
+			end;
+			
+		end;
+		predictions_exp{ex} = predictions;
+		clear predictions;
+	end;
+	save(['dataset/experiment1/u_data_predictions_exp',num2str(nn),'.mat'],'predictions_exp','-mat');
+	clear predictions_std;
+end;
+
+finish = clock;
