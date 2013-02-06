514d46e2a2a0022f64986a97408f73aa26132ef7
Ricardo Erikson <ricardoerikson@gmail.com>
2013-02-06 14:49:39 -0400

    Initial commit experiment 2. Rename filenames

--- main_u_data_correlation.m
+++ main_u_data_correlation.m
@@ -4,6 +4,13 @@ clc;
 
 configure
 
+mkdir(['dataset/' experiment_name]);
+
+% Configuração: 50% treinamento e 50% teste
+
+p_training = 0.5;
+p_probe = 0.5;
+
 n_users = 943;
 n_items = 1682;
 
@@ -17,7 +24,7 @@ parfor i=1:n_users
 
 	all_observed = find(ratings_matrix(i,:) > 0);
 	n_observed = max(size(all_observed));
-	n_probe = round(0.2*n_observed);
+	n_probe = round(p_probe*n_observed);
 
 	xseq = randperm(n_observed);
 
@@ -31,4 +38,4 @@ end;
 
 [training_correlation,training_ratings] = correlation('dataset/u.data',n_users,n_items,0,probe_set);
 
-save('dataset/u_data_experiment_1.mat','non_observed_set','training_correlation','training_ratings','probe_set','training_set','ratings_matrix','-mat');
+save(['dataset/' experiment_name '/u_data.mat'],'non_observed_set','training_correlation','training_ratings','probe_set','training_set','ratings_matrix','-mat');
