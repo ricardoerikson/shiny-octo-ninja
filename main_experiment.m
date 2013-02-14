ee8410b95037495b5d0781104c30ce7828d5ba03
Ricardo Erikson <ricardoerikson@gmail.com>
2013-02-14 08:00:50 -0400

    Calculating correlation and prediction

--- main_experiment.m
+++ main_experiment.m
@@ -1,4 +1,12 @@
-main_u_data_correlation
+
+% Calculating similarity
+configure;
+mkdir(['dataset/' experiment_name]);
+load('dataset/u_data_t80_p20.mat');
+[training_correlation,training_ratings] = correlation('dataset/u.data',n_users,n_items,1,probe_set);
+save(['dataset/' experiment_name '/u_data.mat'],'non_observed_set','training_correlation','training_ratings','probe_set','training_set','ratings_matrix','-mat');
+
+% Calculating predictions
 main_u_data_prediction_std
 main_u_data_prediction_exp
 main_u_data_prediction_wgt
