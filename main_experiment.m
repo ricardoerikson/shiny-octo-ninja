0940b6d715034f9f747b6c9c03124892ac148b68
Ricardo Erikson <ricardoerikson@gmail.com>
2013-02-14 14:17:38 -0400

    main_experiment using dataset/u_data_t20_p80.mat

--- main_experiment.m
+++ main_experiment.m
@@ -5,7 +5,7 @@ configure;
 n_users = 943;
 n_items = 1682;
 mkdir(['dataset/' experiment_name]);
-load('dataset/u_data_t80_p20.mat');
+load('dataset/u_data_t20_p80.mat');
 [training_correlation,training_ratings] = correlation('dataset/u.data',n_users,n_items,1,probe_set);
 save(['dataset/' experiment_name '/u_data.mat'],'non_observed_set','training_correlation','training_ratings','probe_set','training_set','ratings_matrix','-mat');
 
